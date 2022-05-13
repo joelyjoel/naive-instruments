c1c4bedbd9bdd52e7c089ee5459e41b2a807b839
Joelle Plow <joelleplow@pm.me>
2022-05-13 23:35:29 +0100

    class Multiply TODO: Untested

---
+++ src/Multiply.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "NaiveInstrument.h"
+#include "Socket.h"
+
+class Multiply : public NaiveInstrument<double> {
+public:
+  Socket<double> &a = addSocket<double>();
+  Socket<double> &b = addSocket<double>();
+
+  template <typename T, typename U> Multiply(T leftOperand, U rightOperand) {
+    a = leftOperand;
+    b = rightOperand;
+  }
+  double tick() { return a() * b(); }
+};
