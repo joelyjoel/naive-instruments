69b5f6d36ded44563657395cdeab28085a21d63b
Joelle Plow <joelleplow@pm.me>
2022-05-15 02:43:10 +0100

    Setup lfs + commit test snapshots

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,11 @@
-#include "./makeASawToothWave.h"
-#include "Add.h"
-#include "BreakpointEnvelope.h"
-#include "Osc.h"
-#include "WavWriter.h"
-#include "Waveforms.h"
-#include "Wavetable.h"
-#include "record.h"
-#include <iostream>
-#include <ostream>
+#include "TestUtils.h"
+#include "lib.h"
 
 int main() {
 
-  BreakpointEnvelope envelope;
-  for (int i = 0; i < 4; ++i) {
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .1);
-    envelope.addSection(400, 0, .3);
-    envelope.addSection(400, 0, .3);
-    envelope.addSection(400, 0, .2);
-  }
-
-  Osc signal(200, Waveforms::sine());
-  signal.frequency = envelope;
-
-  record("output.wav", signal, 2);
+  AudioSnapshotTest test1("a sine wave", new Osc(440));
+  AudioSnapshotTest test2("a square wave", new Square(100));
+  AudioSnapshotTest test3("a sawtooth wave", new Saw(100));
 
   return 0;
 }
