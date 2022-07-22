4cff4a781d3670e821c8c05d4d9545c06ef2b9ad
Joelle Plow <joelleplow@pm.me>
2022-07-22 14:39:40 +0100

    fm output routings

--- src/apps/FMApp.h
+++ src/apps/FMApp.h
@@ -10,8 +10,21 @@ public:
 
     // Create the oscs
     std::vector<Osc *> oscs;
-    for (int i = 0; i < numberOfOscs; ++i)
-      oscs.push_back(new Sine());
+    std::vector<NaiveInstrument<double> *> outputs;
+    for (int i = 0; i < numberOfOscs; ++i) {
+      Osc *osc = new Sine;
+      oscs.push_back(osc);
+      const std::string key = std::to_string(i);
+      if (args.exists(key)) {
+        NaiveInstrument<double> *level = args.signal(key);
+        Multiply *m = new Multiply;
+        m->a << osc;
+        m->b << level;
+        outputs.push_back(m);
+      } else {
+        outputs.push_back(osc);
+      }
+    }
 
     // Assign the fundamental frequencies
     std::vector<Socket<double> *> pitchSockets;
@@ -29,12 +42,12 @@ public:
     for (int carrierIndex = 0; carrierIndex < numberOfOscs; ++carrierIndex) {
       for (int modulatorIndex = 0; modulatorIndex < numberOfOscs;
            ++modulatorIndex) {
-        const std::string key =
-            std::to_string(carrierIndex) + "-" + std::to_string(modulatorIndex);
+        const std::string key = std::to_string(modulatorIndex) + "to" +
+                                std::to_string(carrierIndex);
         if (args.exists(key)) {
           NaiveInstrument<double> *modulation = args.signal(key);
           Multiply *m = new Multiply();
-          m->a << oscs[modulatorIndex];
+          m->a << outputs[modulatorIndex];
           m->b << modulation;
           std::cout << "Modulating Osc" << carrierIndex << " by Osc"
                     << modulatorIndex << "\n";
@@ -55,16 +68,25 @@ public:
       }
     }
 
+    // Create the final mix
     NaiveInstrument<double> *mixdown = nullptr;
     for (int i = 0; i < numberOfOscs; ++i) {
-      Osc *osc = oscs[i];
-      if (mixdown == nullptr) {
-        mixdown = osc;
-      } else {
-        Add *add = new Add();
-        add->a << mixdown;
-        add->b << osc;
-        mixdown = add;
+      NaiveInstrument<double> *oscOutput = outputs[i];
+      const std::string key = std::to_string(i) + "-out";
+      if (args.exists(key)) {
+        NaiveInstrument<double> *level = args.signal(key, "0");
+        Multiply *m = new Multiply;
+        m->a << oscOutput;
+        m->b << level;
+
+        if (mixdown == nullptr) {
+          mixdown = m;
+        } else {
+          Add *add = new Add();
+          add->a << mixdown;
+          add->b << m;
+          mixdown = add;
+        }
       }
     }
 
