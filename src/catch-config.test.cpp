57619af44c64d74dff48bb777c0f271bf0562ef0
Joelle Plow <joelleplow@pm.me>
2022-07-28 10:37:56 +0100

    Move test config to root of src/

---
+++ src/catch-config.test.cpp
@@ -0,0 +1,6 @@
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
+                          // in one cpp file
+
+#include "../dependencies/catch.hpp"
+
+TEST_CASE("Need a dummy test case to make catch work") { REQUIRE(true); }
--- src/parsing/parse.test.cpp
+++ src/parsing/parse.test.cpp
@@ -1,6 +1,4 @@
 
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
-                          // in one cpp file
 #include "../../dependencies/catch.hpp"
 
 #include "Parse.h"
