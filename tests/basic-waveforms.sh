9f2fe4a2d8a853f9fe5e3302fec085c4e8861da1
Joelle Plow <joelleplow@pm.me>
2022-08-06 22:24:12 +0100

    Update the renamed command name in tests!

--- tests/basic-waveforms.sh
+++ tests/basic-waveforms.sh
@@ -1,11 +1,11 @@
 #! /usr/local/bin/bash
 
-ni osc --pitch 60 --duration 1s > "Sine wave on middle C lasting one second.wav"
-ni osc --waveform saw --pitch 60 --duration 1s > "Sawtooth wave on middle C lasting one second.wav"
-ni osc --waveform square --pitch 60 --duration 1s > "Square wave on middle C lasting one second.wav"
-ni osc --waveform triangle --pitch 60 --duration 1s > "Triangle wave on middle C lasting one second.wav"
-ni osc --waveform saw --pitch 21 --duration 1s > "Bass sawtooth wave.wav"
+malc osc --pitch 60 --duration 1s > "Sine wave on middle C lasting one second.wav"
+malc osc --waveform saw --pitch 60 --duration 1s > "Sawtooth wave on middle C lasting one second.wav"
+malc osc --waveform square --pitch 60 --duration 1s > "Square wave on middle C lasting one second.wav"
+malc osc --waveform triangle --pitch 60 --duration 1s > "Triangle wave on middle C lasting one second.wav"
+malc osc --waveform saw --pitch 21 --duration 1s > "Bass sawtooth wave.wav"
 
-ni osc --waveform sine --pitch 108~21 --duration 1s > "Sine sweep accross entire grand piano range.wav"
-ni osc --waveform sine --pitch 21~108 --duration 1s > "Upward sine sweep accross entire grand piano range.wav"
-ni osc --pitch 60 --va 2 --duration 2s > "Middle C Sine wave with Vibrato.wav"
+malc osc --waveform sine --pitch 108~21 --duration 1s > "Sine sweep accross entire grand piano range.wav"
+malc osc --waveform sine --pitch 21~108 --duration 1s > "Upward sine sweep accross entire grand piano range.wav"
+malc osc --pitch 60 --va 2 --duration 2s > "Middle C Sine wave with Vibrato.wav"
--- tests/fm-synthesis.sh
+++ tests/fm-synthesis.sh
@@ -1,5 +1,5 @@
 #! bin/bash
 
-ni fm 60 2 --1to0 24 --duration 2 > "Simple FM tone.wav"
+malc fm 60 2 --1to0 24 --duration 2 > "Simple FM tone.wav"
 
-ni fm 100ms:90~40~~~30~~20\ 1s:~10 40 --1to0 50ms:30~0 --0-out 1~~0 --duration  2.5 > "FM Kick.wav"
+malc fm 100ms:90~40~~~30~~20\ 1s:~10 40 --1to0 50ms:30~0 --0-out 1~~0 --duration  2.5 > "FM Kick.wav"
