f9181f4a9cf1777310380504111d361d92b8c515
Joelle Plow <joelleplow@pm.me>
2022-08-07 19:06:25 +0100

    UnsignedSaw can resetState

--- src/instruments/UnsignedSaw.h
+++ src/instruments/UnsignedSaw.h
@@ -8,7 +8,7 @@ public:
   SignalInput<double> &frequency = addInput<double>();
   double phase;
 
-  void action() {
+  void action() override {
     phase += frequency() / sampleRate;
     while (phase > 1)
       phase -= 1.0;
@@ -17,5 +17,7 @@ public:
     out(phase);
   }
 
-  std::string label() { return "UnsignedSaw"; }
+  void resetState() override { phase = 0; }
+
+  std::string label() override { return "UnsignedSaw"; }
 };
