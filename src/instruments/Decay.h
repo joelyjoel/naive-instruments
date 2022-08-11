fa99000a275dcae758b3fbe283dd40fbff8dd0c5
Joelle Plow <joelleplow@pm.me>
2022-08-11 19:32:23 +0100

    new signal:  Decay

---
+++ src/instruments/Decay.h
@@ -0,0 +1,18 @@
+#include "../core.h"
+
+class Decay : public Signal<double> {
+private:
+  double phase = 0;
+
+public:
+  SignalInput<double> &duration = addInput<double>(1.0);
+
+protected:
+  void action() override {
+    if (phase < 1.0) {
+      phase += (1.0 / 44100.0) / duration();
+      out(1.0 - phase);
+    } else
+      out(0.0);
+  }
+};
