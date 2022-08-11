b33a134dfb2d06af6ca9587f93995cfd620f216c
Joelle Plow <joelleplow@pm.me>
2022-08-11 19:38:15 +0100

    Add tests for boop!

--- tests/basic-waveforms.sh
+++ tests/basic-waveforms.sh
@@ -9,3 +9,9 @@ malc osc --waveform saw --pitch 21 --duration 1s > "Bass sawtooth wave.wav"
 malc osc --waveform sine --pitch 108~21 --duration 1s > "Sine sweep accross entire grand piano range.wav"
 malc osc --waveform sine --pitch 21~108 --duration 1s > "Upward sine sweep accross entire grand piano range.wav"
 malc osc --pitch 60 --va 2 --duration 2s > "Middle C Sine wave with Vibrato.wav"
+
+
+malc boop --duration 1 > "Simple boop.wav"
+malc boop --duration 5 > "Slightly longer boop.wav"
+malc boop --duration 1 --waveform triangle > "Trianglular boop.wav"
+malc boop --duration 1 --pitch 50~0 > "Sliding boop.wav"
