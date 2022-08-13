138c5dfce4ccabcddf137bb4a98e2482187e7da0
Joelle Plow <joelleplow@pm.me>
2022-08-13 08:41:20 +0100

    Add label for PitchConverter

--- src/instruments/PitchConverter.h
+++ src/instruments/PitchConverter.h
@@ -13,5 +13,7 @@ class PitchConverter : public Signal<double> {
 public:
   SignalInput<double> &pitch = addInput<double>();
 
-  void action() { out(440 * pow(2.0, ((pitch() - 69) / 12.0))); }
+  void action() override { out(440 * pow(2.0, ((pitch() - 69) / 12.0))); }
+
+  std::string label() override { return "PitchConverter"; }
 };
