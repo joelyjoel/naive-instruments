b51b3daa012c3703d4daa0a23ff2ccb1b9e60ff6
Joelle Plow <joelleplow@pm.me>
2022-08-13 08:47:41 +0100

    Add label to Multiply signal

--- src/instruments/Multiply.h
+++ src/instruments/Multiply.h
@@ -7,7 +7,8 @@ public:
   SignalInput<double> &a = addInput<double>();
   SignalInput<double> &b = addInput<double>();
 
-  void action() { out(a() * b()); }
+  void action() override { out(a() * b()); }
+  std::string label() override { return "Multiply"; }
 };
 
 /**
