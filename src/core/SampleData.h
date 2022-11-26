e96c78b47d6d2dfdb2121ac7b2d9496bf2294589
Joelle Plow <joelleplow@pm.me>
2022-11-26 13:48:22 +0000

    Rename SampleData

---
+++ src/core/SampleData.h
@@ -0,0 +1,174 @@
+
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <memory>
+#include <vector>
+
+template <typename T> class SampleData {
+
+  int numberOfFrames;
+  int numberOfChannels;
+  T *data;
+
+public:
+  SampleData(int numberOfFrames, int numberOfChannels = 1)
+      : numberOfChannels(numberOfChannels), numberOfFrames(numberOfFrames) {
+    data = new T[numberOfChannels * numberOfFrames];
+  }
+
+  SampleData(std::vector<T> _data, int numberOfChannels = 1)
+      : SampleData(_data.size(), numberOfChannels) {
+    for (int i = 0; i < _data.size(); ++i)
+      data[i] = _data[i];
+  }
+
+  ~SampleData() { delete data; }
+
+private:
+  /**
+   * Get a reference to a specific cell of the sample data.
+   */
+  T &cell(int channel, int frame) {
+    // TODO: add bounds checks
+    if (frame < 0 || frame >= numberOfFrames) {
+      std::cerr << "frame out of bounds: " << frame << "\n";
+      throw 1; // TODO: Use proper exception
+    } else if (channel < 0 || channel >= numberOfChannels) {
+      std::cerr << "channel out of bonuds: " << channel << "\n";
+      throw 1; // TODO: Use proper exception
+    }
+
+    return data[frame * numberOfChannels + channel];
+  }
+
+  // TODO: Define an iterator through a region of neighboring data
+
+public:
+  T read(int frame, int channel = 0) { return cell(channel, frame); }
+
+  T readWithInterpolation(float frame, int channel = 0) {
+    // (linear interpolation between samples)
+    float progress = fmod(frame, 1);
+    if (progress == 0)
+      return read(int(frame), channel);
+    T before = read(int(frame), channel);
+    T after = read(int(frame + 1), channel);
+    return (1.0 - progress) * before + progress * after;
+  }
+
+  /**
+   * Overwrite a single cell
+   */
+  void write(T y, int frame, int channel = 0) { cell(channel, frame) = y; }
+
+  /**
+   * Overwrite a region using another sample
+   */
+  void write(SampleData<T> &y, int offset = 0) {
+    // TODO: Support channel offset
+    for (int c = 0; c < y.numberOfChannels; ++c)
+      for (int readFrame = 0; readFrame < y.numberOfFrames; ++readFrame) {
+        int writeFrame = readFrame + offset;
+        if (writeFrame >= 0 && writeFrame < numberOfFrames)
+          write(y.read(readFrame, c), writeFrame, c);
+      }
+  }
+
+  void mix(T y, int frame = 0, int channel = 0) { cell(channel, frame) += y; }
+
+  void mix(SampleData<T> &y, int offset = 0) {
+    for (int c = 0; c < y.numberOfChannels; ++c)
+      for (int readFrame = 0; readFrame < y.numberOfFrames; ++readFrame) {
+        int writeFrame = readFrame + offset;
+        if (writeFrame >= 0 && writeFrame < numberOfFrames)
+          mix(y.read(readFrame, c), writeFrame, c);
+      }
+  }
+
+  T peak(int frame0 = 0, int frame1 = 0) {
+    if (frame1 == 0)
+      frame1 = numberOfFrames;
+
+    T max = read(frame0, 0);
+    for (int c = 0; c < numberOfChannels; ++c)
+      for (int frame = frame0; frame < frame1; ++frame)
+        max = std::max(max, read(frame, c));
+    return max;
+  }
+
+  T rms(int frame0 = 0, int frame1 = 0) {
+    if (frame1 == 0)
+      frame1 = numberOfFrames;
+
+    T sum = 0;
+    for (int frame = frame0; frame < frame1; ++frame)
+      for (int channel = 0; channel < numberOfChannels; ++channel) {
+        T y = read(frame, channel);
+        sum += y * y;
+      }
+
+    int n = (frame1 - frame0) * numberOfChannels;
+    return sqrt(sum / n);
+  }
+
+  std::shared_ptr<SampleData<T>> slice(int frame0 = 0, int frame1 = 0) {
+    if (frame1 == 0)
+      frame1 = numberOfFrames;
+
+    int lengthOfNewSample = frame1 - frame0;
+
+    auto sample =
+        std::make_shared<SampleData<T>>(lengthOfNewSample, numberOfChannels);
+
+    for (int writeFrame = 0; writeFrame < lengthOfNewSample; ++writeFrame) {
+      int readFrame = writeFrame + frame0;
+      for (int channel = 0; channel < numberOfChannels; ++channel)
+        sample->write(read(readFrame, channel), writeFrame, channel);
+    }
+
+    return sample;
+  }
+
+  // TODO: writing wav files
+  // TODO: Reading wav files
+
+  std::shared_ptr<SampleData<T>>
+  selectChannels(std::vector<int> &channelsToSelect) {
+    auto newSample = std::make_shared<SampleData<T>>(numberOfFrames,
+                                                     channelsToSelect.size());
+    for (int writeChannel = 0; writeChannel < channelsToSelect.size();
+         ++writeChannel)
+      for (int frame = 0; frame < numberOfFrames; ++frame)
+        newSample->write(read(frame, channelsToSelect[writeChannel]), frame,
+                         writeChannel);
+    return newSample;
+  }
+
+  std::shared_ptr<SampleData<T>> stereoFlip() {
+    std::vector<int> channels = {1, 0};
+    if (numberOfChannels == 2)
+      return selectChannels(channels);
+    else
+      throw 1; // TODO: Use proper exception
+  }
+
+  std::shared_ptr<SampleData<T>> selectChannel(int channel) {
+    std::vector<int> channels = {channel};
+    return selectChannels(channels);
+  }
+
+  static std::shared_ptr<SampleData<T>> concat(SampleData<T> &a,
+                                               SampleData<T> &b) {
+    int numberOfChannels = std::max(a.numberOfChannels, b.numberOfChannels);
+    int numberOfFrames = a.numberOfFrames + b.numberOfFrames;
+    std::shared_ptr<SampleData<T>> sample =
+        std::make_shared<SampleData<T>>(numberOfFrames, numberOfChannels);
+
+    // TODO: add configurable crossfades
+
+    sample->write(a, 0);
+    sample->write(b, a.numberOfFrames);
+    return sample;
+  }
+};
---
+++ src/core/SampleData.test.cpp
@@ -0,0 +1,102 @@
+
+#include "./SampleData.h"
+#include "../../dependencies/catch.hpp"
+
+TEST_CASE("reading and writing to a sample") {
+
+  SampleData<bool> mySample(8);
+  mySample.write(true, 0, 0);
+  REQUIRE(mySample.read(0, 0) == true);
+};
+
+// TODO: Bounds errors are thrown when accessing out of bounds
+// TODO: Reading and writing multichannel samples
+// TODO: Tests including large amounts of data
+
+TEST_CASE("Create a sample from a vector and find the peak") {
+  REQUIRE(SampleData<int>({1, 2, 3, 4}).peak() == 4);
+}
+
+TEST_CASE("Root mean square of a sample") {
+  REQUIRE(SampleData<int>({1, 1, 1, 1}).rms() == 1);
+}
+
+TEST_CASE("Overwrite a region of one sample using another") {
+  SampleData<int> master(8);
+  SampleData<int> overdub({1, 2, 3, 4});
+  master.write(overdub);
+  REQUIRE(master.read(1) == 2);
+};
+
+TEST_CASE("Overrwriting a region using another sample and an offset") {
+  SampleData<int> master(8);
+  SampleData<int> overdub({1, 2, 3, 4});
+  master.write(overdub, 2);
+  REQUIRE(master.read(1) == 0);
+  REQUIRE(master.read(2) == 1);
+  REQUIRE(master.read(5) == 4);
+}
+
+TEST_CASE("Concatenating two samples") {
+  SampleData<int> first({1, 2, 3, 4});
+  SampleData<int> second({5, 6, 7, 8});
+
+  auto combined = SampleData<int>::concat(first, second);
+  REQUIRE(combined->read(7) == 8);
+  REQUIRE(combined->read(0) == 1);
+}
+
+TEST_CASE("Mixing two samples together") {
+  SampleData<int> master({1, 1, 1, 1, 1});
+  SampleData<int> overdub({0, 1, 2, 3, 4});
+
+  master.mix(overdub);
+
+  REQUIRE(master.read(0) == 1);
+  REQUIRE(master.read(1) == 2);
+  REQUIRE(master.read(2) == 3);
+  REQUIRE(master.read(3) == 4);
+  REQUIRE(master.read(4) == 5);
+}
+
+TEST_CASE("Slicing a sample") {
+  SampleData<int> master({0, 1, 2, 3, 4, 5, 6, 7, 8}, 1);
+  auto slice = master.slice(2, 4);
+  REQUIRE(slice->read(0) == 2);
+  REQUIRE(slice->read(1) == 3);
+}
+
+TEST_CASE(
+    "Reading between the samples, linear interpolation should be applied") {
+  SampleData<float> sample({0, 1, 2, 3});
+  REQUIRE(sample.readWithInterpolation(0.0) == 0);
+  REQUIRE(sample.readWithInterpolation(0.5) == .5);
+  REQUIRE(sample.readWithInterpolation(3.0) == 3);
+}
+
+TEST_CASE("Selecting a single channel from a multichannel sample") {
+  SampleData<int> stereo({0, 1, 2, 3, 4, 5, 6}, 2);
+  auto rightChannel = stereo.selectChannel(1);
+  REQUIRE(rightChannel->read(0) == 1);
+  REQUIRE(rightChannel->read(1) == 3);
+  REQUIRE(rightChannel->read(2) == 5);
+}
+
+TEST_CASE("Stereo flip using selectChannels") {
+  SampleData<int> stereo({0, 1, 2, 3, 4, 5, 6}, 2);
+  std::vector<int> channels = {1, 0};
+  auto flipped = stereo.selectChannels(channels);
+  REQUIRE(flipped->read(0) == 1);
+  REQUIRE(flipped->read(0, 1) == 0);
+  REQUIRE(flipped->read(1, 0) == 3);
+  REQUIRE(flipped->read(1, 1) == 2);
+}
+
+TEST_CASE("Stereo flip using dedicated stereoFlip method") {
+  SampleData<int> stereo({0, 1, 2, 3, 4, 5, 6}, 2);
+  auto flipped = stereo.stereoFlip();
+  REQUIRE(flipped->read(0) == 1);
+  REQUIRE(flipped->read(0, 1) == 0);
+  REQUIRE(flipped->read(1, 0) == 3);
+  REQUIRE(flipped->read(1, 1) == 2);
+}
