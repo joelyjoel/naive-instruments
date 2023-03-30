454805256bcfa7ee8d1f1312873c46f1ce8054fd
Joelle Plow <joelleplow@pm.me>
2023-03-30 13:38:44 +0100

    Move FrameStream to its own file

---
+++ src/core/FrameStream.h
@@ -0,0 +1,46 @@
+#include "AbstractFrameStream.h"
+
+template <typename frame> class FrameStream : public AbstractFrameStream {
+protected:
+  frame *output;
+
+public:
+  FrameStream() { output = new frame; }
+  ~FrameStream() { delete output; }
+
+protected:
+  /**
+   * Update the latest frame.
+   */
+  void writeFrame(const frame &y) { *output = y; }
+
+public:
+  frame operator[](int clock) {
+    sync(clock);
+    return readFrame();
+  }
+
+  frame readFrame() { return *output; }
+
+  frame advanceToNextFrameAndRead() {
+    advanceToNextFrame();
+    return readFrame();
+  }
+
+  AbstractFrameStreamConsumer &defaultInput() {
+    if (inputs.size() > 0)
+      return *inputs[0];
+    else {
+      std::cerr << "Trying to get default input but the Signal has no inputs";
+      throw 1;
+    }
+  }
+
+  void operator<<(double k) { defaultInput().setConstant(k); }
+
+public:
+  std::string summary() override {
+    return label() + "[" + std::to_string(internalClock) +
+           "] = " + std::to_string(*output);
+  }
+};
