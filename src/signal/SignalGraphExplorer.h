c0df972380f0c314b04838e85d5739090c7407fd
Joelle Plow <joelleplow@pm.me>
2023-08-27 16:12:54 +0100

    Searching signal graph for unassigned inputs

--- src/signal/SignalGraphExplorer.h
+++ src/signal/SignalGraphExplorer.h
@@ -1,4 +1,5 @@
 #include "Signal.h"
+#include <algorithm>
 #include <memory>
 #include <vector>
 namespace NaiveInstruments
@@ -6,6 +7,9 @@ namespace NaiveInstruments
 class SignalGraphExplorer
 {
 public:
+    /**
+     * List the signals which are a direct input to the given signal.
+     */
     static std::vector<std::shared_ptr<UnknownOutputSignal>> directSignalInputs(
         std::shared_ptr<UnknownOutputSignal> signal )
     {
@@ -15,6 +19,60 @@ public:
             auto sig = input->get_signal();
             if ( sig )
                 list.push_back( sig );
+        }
+        return list;
+    }
+
+    /**
+     * List all the signals which are direct or indirect inputs to the given signal by exploring the graph recursively.
+     */
+    static std::vector<std::shared_ptr<UnknownOutputSignal>> recursiveSignalInputs(
+        std::shared_ptr<UnknownOutputSignal> startingPoint,
+        /**
+         * Should the starting point be included in the results?
+         */
+        bool inclusive = false )
+    {
+        std::vector<std::shared_ptr<UnknownOutputSignal>> list;
+        list.push_back( startingPoint );
+        for ( int i = 0; i < list.size(); ++i )
+        {
+            auto current      = list[i];
+            auto directInputs = directSignalInputs( current );
+            for ( auto directInput : directInputs )
+                if ( !std::count( list.begin(), list.end(), directInput ) )
+                    list.push_back( directInput );
+        }
+
+        if ( !inclusive )
+            list.erase( list.begin() );
+
+        return list;
+    }
+
+    static std::vector<AbstractManagedAccessor*> listUnassignedAccessors( std::shared_ptr<UnknownOutputSignal> signal )
+    {
+        std::vector<AbstractManagedAccessor*> list;
+        for ( auto accessor : signal->inputs )
+            if ( !accessor->has_been_set_manually )
+                list.push_back( accessor );
+        return list;
+    }
+
+    /**
+     * List all the unassigned managed accessors in a signal graph
+     */
+    static std::vector<AbstractManagedAccessor*> recursivelyListUnassignedAccessors(
+        std::shared_ptr<UnknownOutputSignal> startingPoint )
+    {
+        static std::vector<AbstractManagedAccessor*> list;
+        auto                                         allProcesses = recursiveSignalInputs( startingPoint, true );
+        for ( auto process : allProcesses )
+        {
+            for ( auto accessor : listUnassignedAccessors( process ) )
+            {
+                list.push_back( accessor );
+            }
         }
         return list;
     }
--- src/signal/SignalGraphExplorer.test.cpp
+++ src/signal/SignalGraphExplorer.test.cpp
@@ -2,10 +2,11 @@
 #include "../test-framework/custom-assertions.h"
 #include "./SignalShorthands.h"
 
+using namespace NaiveInstruments;
+using namespace SignalShorthands;
+
 TEST_CASE( "Listing the direct inputs of a signal" )
 {
-    using namespace NaiveInstruments;
-    using namespace SignalShorthands;
     auto mySum = std::make_shared<Sum<double>>();
 
     CHECK( SignalGraphExplorer::directSignalInputs( mySum ).size() == 0 );
@@ -14,4 +15,22 @@ TEST_CASE( "Listing the direct inputs of a signal" )
     CHECK( SignalGraphExplorer::directSignalInputs( mySum ).size() == 1 );
     mySum->input2 = sine( 440 );
     CHECK( SignalGraphExplorer::directSignalInputs( mySum ).size() == 2 );
+}
+
+TEST_CASE( "Recursively listing the signals which are an input to the given signal" )
+{
+    auto signal = noise() + noise() + noise();
+
+    auto allProcesses = SignalGraphExplorer::recursiveSignalInputs( signal, true );
+    CHECK( allProcesses.size() == 5 );
+}
+
+TEST_CASE( "Finding unassigned inputs" )
+{
+    auto signal = noise() + noise() + noise();
+    CHECK( SignalGraphExplorer::recursivelyListUnassignedAccessors( signal ).size() == 0 );
+
+    auto mySum = std::make_shared<Sum<double>>();
+    signal     = noise() + noise() + mySum;
+    CHECK( SignalGraphExplorer::recursivelyListUnassignedAccessors( signal ).size() == 2 );
 }
