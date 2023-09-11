066df0db05eb773eecb8df72489f3dd5e03a63bf
Joelle Plow <joelleplow@pm.me>
2023-09-11 18:55:28 +0100

    Move reference tones to root level directory

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -1,18 +1,19 @@
 {
   cd $(dirname $0)
+  referenceToneLocation="../reference-tones"
 
   for file in *.test.sh ; do
     bash $file
   done
 
 
-  mkdir -p results
+  mkdir -p "$referenceToneLocation"
   for sample in *.wav ; do
-    mv "$sample" results
+    mv "$sample" "$referenceToneLocation"
   done
 
   source "./checkaudio.sh"
-  for sample in results/*.wav ; do 
+  for sample in $referenceToneLocation/*.wav ; do 
     checkaudio "$sample" || exit 1
   done
 }
