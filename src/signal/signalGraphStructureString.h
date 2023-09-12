395aeafd0fc646c4fabf657d85c648278ca20eb6
Joelle Plow <joelleplow@pm.me>
2023-09-12 15:14:39 +0100

    Manually shorthand frequent signal processes
    This is not a long term viable solution. But it demonstrates the proof
    of concept.

--- src/signal/signalGraphStructureString.h
+++ src/signal/signalGraphStructureString.h
@@ -1,5 +1,6 @@
 #include "Signal.h"
 #include "SignalShorthands.h"
+#include "signal-processes.h"
 #include <boost/algorithm/string.hpp>
 #include <string>
 
@@ -10,9 +11,31 @@ inline std::string indentInPlace( std::string str, int numberOfSpaces = 2 )
     return spaces + str;
 }
 
+
+inline std::string signalProcessName( std::shared_ptr<NaiveInstruments::UnknownOutputSignal> signal )
+{
+    using namespace NaiveInstruments;
+    NaiveInstruments::UnknownOutputSignal& s = *signal;
+    std::string                            t = typeid( s ).name();
+    if ( t == typeid( NaiveInstruments::Constant<double> ).name() )
+        return std::to_string( ( (NaiveInstruments::Constant<double>*) &s )->output );
+    if ( t == typeid( NaiveInstruments::Multiply ).name() )
+        return "*";
+    if ( t == typeid( NaiveInstruments::LinearRamp ).name() )
+        return "ramp";
+    if ( t == typeid( NaiveInstruments::Sum<double> ).name() )
+        return "+";
+    if ( t == typeid( NaiveInstruments::USaw ).name() )
+        return "usaw";
+    if ( t == typeid( NaiveInstruments::Wavetable ).name() )
+        return "wavetable";
+
+    return typeid( s ).name();
+}
+
 inline std::string signalGraphStructureString( std::shared_ptr<NaiveInstruments::UnknownOutputSignal> signal )
 {
-    std::string str = typeid( *signal ).name();
+    std::string str = signalProcessName( signal );
     for ( auto input : signal->inputs )
     {
         str += "\n" + indentInPlace( signalGraphStructureString( input->abstract_ptr() ) );
