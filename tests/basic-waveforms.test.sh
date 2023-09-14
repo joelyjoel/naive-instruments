43c666826236c8879de7d864cd8dccdca7402789
Joelle Plow <joelleplow@pm.me>
2023-09-14 10:36:21 +0100

    Remove e2e tests that use control strings

--- tests/basic-waveforms.test.sh
+++ tests/basic-waveforms.test.sh
@@ -10,10 +10,6 @@ naive osc --waveform triangle --pitch 60 --duration 1s > "Triangle wave on middl
 
 naive osc --waveform saw --pitch 21 --duration 1s > "Bass sawtooth wave.wav"
 
-naive osc --waveform sine --pitch 108~21 --duration 1s > "Sine sweep accross entire grand piano range.wav"
-
-naive osc --waveform sine --pitch 21~108 --duration 1s > "Upward sine sweep accross entire grand piano range.wav"
-
 naive osc --pitch 60 --va 2 --duration 2s > "Middle C Sine wave with Vibrato.wav"
 
 naive boop --duration 1 > "Simple boop.wav"
@@ -22,4 +18,3 @@ naive boop --duration 5 > "Slightly longer boop.wav"
 
 naive boop --duration 1 --waveform triangle > "Trianglular boop.wav"
 
-naive boop --duration 1 --pitch 50~0 > "Sliding boop.wav"
